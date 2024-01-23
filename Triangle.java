ca840eb3d83a2b9e6b70331116e0bccc30e309ab
Gaia Sofer <gaia.sofer@post.runi.ac.il>
2024-01-23 22:19:46 +0200

    updated 23/01/24 22:19

--- Triangle.java
+++ Triangle.java
@@ -5,14 +5,13 @@
  */ 
 public class Triangle {	
 	public static void main(String[] args) {
-			int a = Integer.parseInt(args[0]);
-			int b = Integer.parseInt(args[1]);
-			int c = Integer.parseInt(args[2]);
-	
-			int leftSide2 = (a * a) + (b * b);
-			int rightSide2 = c * c;
-			boolean answer = (leftSide2 == rightSide2);
-	
+		int a = Integer.parseInt(args[0]);
+		int b = Integer.parseInt(args[1]);
+		int c = Integer.parseInt(args[2]);
+
+		int leftside = (a + b);
+		boolean answer = (leftside > c);
+		
 			System.out.println(a + ", " + b + ", " + c + ": " + answer);
 		}
 	}
