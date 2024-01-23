0d8f2ee6dbd4bd0ca95228ecd191a60cf81c6bea
Gaia Sofer <gaia.sofer@post.runi.ac.il>
2024-01-23 17:25:45 +0200

    updated 23/01/24 17:25

--- AddTwo.java
+++ AddTwo.java
@@ -3,9 +3,10 @@
  */
 public class AddTwo {
 	public static void main(String[] args) {
+		
 		int num = Integer.parseInt(args[0]);
 		int num1 = Integer.parseInt(args[1]);
 
-		System.out.println(num + "+" num1 + "=" (num + num1));
+		System.out.println(num + "+"  + num1 + "=" + (num + num1));
 	}
 }
