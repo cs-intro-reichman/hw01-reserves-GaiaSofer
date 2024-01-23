1454ca7f9323853f03c17d196076e39625b26fa4
Gaia Sofer <gaia.sofer@post.runi.ac.il>
2024-01-23 21:36:02 +0200

    updated 23/01/24 21:35

--- GenThree.java
+++ GenThree.java
@@ -9,7 +9,16 @@ public class GenThree {
 	int a = Integer.parseInt(args[0]);
 	int b = Integer.parseInt(args[1]);
 	
-	System.out.println((int) (Math.random() * ( b - a ) + a));
+	int answer1 = (int) (Math.random() * ( b - a ) + a);
+	int answer2 = (int) (Math.random() * ( b - a ) + a);
+	int answer3 = (int) (Math.random() * ( b - a ) + a);
 
+	int min = Math.min(answer1, answer2);
+	int min1 = Math.min(min, answer3);
+
+	System.out.println(answer1);
+	System.out.println(answer2);
+	System.out.println(answer3);
+	System.out.println("The" + " minimal " + " generated " + " number " + " was " + min1);
 	}
 }
