28d1f5398952d0ad908bc1b1063f2f654b782a99
Gaia Sofer <gaia.sofer@post.runi.ac.il>
2024-01-23 20:02:58 +0200

    updated 23/01/24 20:02

--- GenThree.java
+++ GenThree.java
@@ -4,6 +4,15 @@
  */
 public class GenThree {
 	public static void main(String[] args) {
-		// Put your code here	
+	// Computes the  equation ax + b = c	
+
+	double a = Double.parseDouble(args[0]);
+	double b = Double.parseDouble(args[1]);
+	double c = Double.parseDouble(args[2]);	
+
+		double equation = ( (c - b) / a );
+
+		System.out.println(equation);
+
 	}
 }
